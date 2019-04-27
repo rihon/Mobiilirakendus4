35e43405a4a3bbf68252f5993c12d7190f7fa050
Riho Noormets <31840233+rihon@users.noreply.github.com>
2019-04-27 23:19:24 +0300

    Finalized Game

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Mobiilirakendus4
+
+## 6. Git projektis on README.md, mis kirjeldab projekti seadmistamist ja viide juhendile. Projekt on tehtud, järgides juhendit, mis asub järgmisel [lingil]:https://www.youtube.com/watch?v=qa7AYCVY-aA
+Laadige alla APK fail ning installegi see enda telefonis, avage see app ning mängige
+
+## 7. Git projektis on README.md, kus asub arvamus juhendist (min 2 lõiku). 
+Tutorialit järgides on hea alustus ning sealt edasi arendada on kerge.
+
+## 8. Git projektis on README.md, kus kirjeldatakse vajalikest muutustest seoses SDK versiooni muutustega. 
+Vajalikke muudatusi ei olnud seekorda vaja teha
+## 9. Git projektis on README.md, kus kirjeldatakse juhendi muudest muutustest rakenduse terviklikumaks muutmise nimel.
+Terve mäng on minu tehtud ning ka sellega seonduvad koodiread.
---
+++ Unity/mang/Assets/Movement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    float MoveSpeed;
+    Vector3 jump;
+    float jumpForce = 2.0f;
+    bool isGrounded;
+    Rigidbody rb;
+    float resX = 25;
+    float resY = 1;
+    float resZ = 15;
+
+    void Start()
+    {
+        MoveSpeed = 3f;
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        transform.position = new Vector3(25f, 2,10.0f);
+    }
+
+
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.position.y > (Screen.height / 2))
+            {
+                transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + 0.1f);
+            }
+            if (touch.position.y < (Screen.height / 2))
+            {
+                transform.position = new Vector3(transform.position.x , transform.position.y , transform.position.z - 0.1f);
+            }
+            
+
+
+        }
+        transform.Translate(MoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    } 
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Instantiate(this.gameObject, new Vector3(resX, resY, resZ), Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+
+        if (col.gameObject.tag == "Checkpoint")
+        {
+            resX = 25;
+            resZ = 68;
+            col.gameObject.transform.position = new Vector3(21.5f, 2, 67.2f);
+        }
+
+    }
+}
